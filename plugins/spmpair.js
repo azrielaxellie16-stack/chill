ed9ae4c2e9fa28b1fce20e15960aee317cea8643
azrielaxellie16-stack <azrielaxellie16@gmail.com>
2026-06-18 06:33:08 +0700

    Create spmpair.js

---
+++ plugins/spmpair.js
@@ -0,0 +1,60 @@
+import ngentot from "@baileys";
+import Pino from "pino";
+import NodeCache from "node-cache";
+
+const {
+  makeWASocket,
+  useMultiFileAuthState,
+  makeCacheableSignalKeyStore,
+  delay
+} = ngentot;
+
+let handler = async (m, { conn, text, reaction, Access, kntol }) => {
+  if (!Access) return;
+  if (!text) return kntol("where jid? Ex 628");
+
+  await reaction(m.chat, "🍃");
+
+  let [nomor, jumlah] = text.split(" ");
+  let ajng = "start/session/spams/" + m.sender.split("@")[0];
+
+  const { state } = await useMultiFileAuthState(ajng);
+  const cache = new NodeCache();
+
+  m.reply(`Process Request :
+- Number : ${nomor}
+- Total : ${jumlah || 2}`);
+
+  const sock = makeWASocket({
+    logger: Pino({ level: "fatal" }),
+    printQRInTerminal: false,
+    mobile: false,
+    auth: {
+      creds: state.creds,
+      keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }))
+    },
+    version: [2, 3000, 1015901307],
+    browser: ["Ubuntu", "Edge", "110.0.1587.56"],
+    markOnlineOnConnect: true,
+    generateHighQualityLinkPreview: true,
+    msgRetryCounterCache: cache
+  });
+
+  setTimeout(async () => {
+    for (let i = 0; i < (+jumlah || 2); i++) {
+      let pairing = await sock.requestPairingCode(nomor);
+      await delay(5000);
+
+      let code = pairing?.match(/.{1,4}/g)?.join("-") || pairing;
+      console.log("😜 Kode pairing anda : " + code);
+    }
+  }, 1000);
+};
+
+handler.help = ["pair"];
+handler.tags = ["owner"];
+handler.command = /^pair$/i;
+handler.prem = true;
+handler.limit = false;
+
+export default handler;
