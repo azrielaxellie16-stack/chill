ebef48123665276c184bd80799b3a35a6718dd8b
azrielaxellie16-stack <azrielaxellie16@gmail.com>
2026-06-18 06:31:56 +0700

    Update sticker-stickers.js

--- plugins/sticker-stickers.js
+++ plugins/sticker-stickers.js
@@ -1,27 +1,56 @@
-let handler = async (m, { text }) => {
-	let q = m.quoted ? m.quoted : m;
-	let mime = (q.msg || q).mimetype || '';
+import fs from "fs-extra";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import os from "os";
 
-	if (/image|video|webp/.test(mime)) {
-		if ((q.msg?.seconds || q.seconds) > 10) {
-			return m.reply('Video harus berdurasi di bawah 10 detik.');
-		}
+let handler = async (m, { conn, quoted, mime, command }) => {
+  try {
+    const media = quoted?.message?.imageMessage ||
+                  quoted?.message?.videoMessage;
 
-		let media = await q.download();
-		let exif;
-		if (text) {
-			const [packname, author] = text.split(/[,|\-+&]/);
-			exif = { packName: packname || '', packPublish: author || '' };
-		}
-		conn.sendSticker(m.chat, media, m, exif);
-	} else {
-		m.reply('Kirim atau reply media untuk dijadikan stiker.');
-	}
+    if (!media) return m.reply("Reply image/video untuk jadi sticker!");
+
+    let stream = await conn.downloadMediaMessage(quoted);
+    let fileIn = path.join(os.tmpdir(), "input_" + Date.now());
+    let fileOut = path.join(os.tmpdir(), "sticker_" + Date.now() + ".webp");
+
+    fs.writeFileSync(fileIn, stream);
+
+    await new Promise((resolve, reject) => {
+      ffmpeg(fileIn)
+        .outputOptions([
+          "-vcodec",
+          "libwebp",
+          "-vf",
+          "scale=512:512:force_original_aspect_ratio=decrease,fps=15",
+          "-loop",
+          "0",
+          "-q:v",
+          "80",
+          "-preset",
+          "default"
+        ])
+        .toFormat("webp")
+        .save(fileOut)
+        .on("end", resolve)
+        .on("error", reject);
+    });
+
+    await conn.sendMessage(m.chat, {
+      sticker: fs.readFileSync(fileOut)
+    }, { quoted: m });
+
+    fs.unlinkSync(fileIn);
+    fs.unlinkSync(fileOut);
+
+  } catch (e) {
+    console.log(e);
+    m.reply("Error saat convert sticker");
+  }
 };
 
-handler.help = ['sticker'];
-handler.tags = ['sticker'];
-handler.command = /^s(tic?ker)?(gif)?$/i;
-handler.register = true;
+handler.help = ["sticker", "s"];
+handler.tags = ["sticker"];
+handler.command = /^stic?ker|s$/i;
 
 export default handler;
